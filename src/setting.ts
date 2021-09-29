d705b183d685dccce75a7519f2b923c8ebb11d5e
bbggkkk <bbggkkk@naver.com>
2021-09-30 01:34:31 +0900

    초기 세팅값 변경

--- src/setting.ts
+++ src/setting.ts
@@ -1,6 +1,6 @@
 const setting:Setting = {
     worldSize : [48, 48],   // [ x size , y size ]
-    tickTime  : 1000,          // number ms (1000 === 1000ms === 1s)
+    tickTime  : 50,          // number ms (1000 === 1000ms === 1s)
     
     backgroundColor : '#fff',
     unitColor :'rgba(222,80,50,1)'
