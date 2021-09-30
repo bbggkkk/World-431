b9418f664fa66a8653ee8bbcf1d26116d3cfd10a
bbggkkk <bbggkkk@naver.com>
2021-09-30 22:29:57 +0900

    빌드

--- src/setting.ts
+++ src/setting.ts
@@ -1,6 +1,6 @@
 const setting:Setting = {
     worldSize : [64, 64],   // [ x size , y size ]
-    tickTime  : 200,          // number ms (1000 === 1000ms === 1s)
+    tickTime  : 50,          // number ms (1000 === 1000ms === 1s)
     
     backgroundColor : '#fff',
     unitColor :'rgba(222,80,50,1)'
