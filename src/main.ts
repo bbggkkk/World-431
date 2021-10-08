438dd3bd8bc63ca8979b8be56432fa76f587023f
bbggkkk <bbggkkk@naver.com>
2021-10-08 03:24:23 +0000

    다시 되돌리기

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,11 @@ import { initWorker }   from './api/worker/Worker';
     const size = x*y;
     const wrap = document.querySelector('#wrapper');
 
-    // window.world    = init(setting);
-    // window.renderer = new Renderer(wrap as HTMLElement, window.world);
-    // window.world.start(window.renderer);
+    window.world    = init(setting);
+    window.renderer = new Renderer(wrap as HTMLElement, window.world);
+    window.world.start(window.renderer);
 
-    const list = getFirstList(x, y);
-    const worker = initWorker((list:Array<number>) => { return list; });
-    worker([1,2,3,4,5,6,7,8,9]).then(data => console.log(data) );
+    // const list = getFirstList(x, y);
+    // const worker = initWorker((list:Array<number>) => { return list; });
+    // worker([1,2,3,4,5,6,7,8,9]).then(data => console.log(data) );
 })();
